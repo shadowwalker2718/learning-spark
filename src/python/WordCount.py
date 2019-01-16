d4075b90317cc48374a3c837324701e479e66095
ShadowWalker <shadowwalker2718@gmail.com>
2019-01-16 07:22:56 -0800

    spark

--- src/python/WordCount.py
+++ src/python/WordCount.py
@@ -3,7 +3,9 @@ import sys
 from pyspark import SparkContext
 
 if __name__ == "__main__":
-    master = "local"
+    #master = "local"
+    master = "yarn"
+    print(sys.argv)
     if len(sys.argv) == 2:
         master = sys.argv[1]
     sc = SparkContext(master, "WordCount")
